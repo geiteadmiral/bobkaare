21fd4984523b0553b9c394cfc80c027b4db26496
geiteadmiral <silje.hennie.johansen@gmail.com>
2024-10-07 13:44:43 +0200

    why bob?

--- scripts.js
+++ scripts.js
@@ -1,15 +1,16 @@
 let sidebar = document.querySelector('.sidebar')
 
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 640) {
+        closeSidebar();
+    }
+});
+
 function openSidebar(){
-    sidebar.style.width = '350px'
+    sidebar.style.width = '300px'
 }
 
 function closeSidebar(){
     sidebar.style.width = "0"
 }
 
-window.addEventListener("resize", () => {
-    if (window.innerWidth >= 640) {
-        closeSidebar();
-    }
-});
