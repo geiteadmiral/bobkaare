c13e859004feab6012844888ca3afc5d3c792b22
geiteadmiral <silje.hennie.johansen@gmail.com>
2024-10-09 15:29:12 +0200

    gfwejjrew

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
+//sidebar -----------------------------------------------------
 let sidebar = document.querySelector('.sidebar')
 
+//lukker sidebaren om skjermen blir 640 piksler eller mindre
 window.addEventListener("resize", () => {
     if (window.innerWidth >= 640) {
         closeSidebar();
     }
 });
 
+//sidebaren endrer bredde avhengig av om den er åpen eller ikke
 function openSidebar(){
     sidebar.style.width = '300px'
 }
